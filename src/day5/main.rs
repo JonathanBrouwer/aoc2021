a7d938b5ffefb1bb5cbd2ff790c21e949208c29d
jonathan <jonathantbrouwer@gmail.com>
2021-12-05 14:58:39 +0100

    Cleanup

--- src/day5/main.rs
+++ src/day5/main.rs
@@ -3,32 +3,32 @@ use std::collections::HashMap;
 use itertools::Itertools;
 
 fn part1(inp: &str) -> usize {
-    let input = parse_input(inp);
-    solve(input.into_iter().filter(|((x1, y1), (x2, y2))| x1 == x2 || y1 == y2).collect())
+    solve(parse_input(inp).into_iter().filter(|((x1, y1), (x2, y2))| x1 == x2 || y1 == y2).collect())
 }
 
 fn part2(inp: &str) -> usize {
-    let input = parse_input(inp);
-    solve(input)
+    solve(parse_input(inp))
 }
 
 fn solve(input: Vec<((isize, isize), (isize, isize))>) -> usize {
     let mut state = HashMap::<(isize, isize), usize>::new();
-    for ((x1, y1), (x2, y2)) in input {
-        let mut x = x1;
-        let mut y = y1;
+    for ((mut x, mut y), (x2, y2)) in input {
         loop {
-            *state.entry((x, y)).or_default() += 1;
+            *state.entry((x, y)).or_insert(0) += 1;
             if x == x2 && y == y2 { break }
             x += (x2 - x).signum();
             y += (y2 - y).signum();
         }
     }
-    state.iter().filter(|(k, v)| **v > 1).count()
+    state.values().filter(|v| **v > 1).count()
 }
 
 fn parse_input(inp: &str) -> Vec<((isize, isize), (isize, isize))> {
-    inp.lines().map(|line| line.split(" -> ").map(|p| p.split(",").map(|n| n.parse().unwrap()).collect_tuple().unwrap()).collect_tuple().unwrap()).collect()
+    inp.lines().map(
+        |line| line.split(" -> ").map(
+            |part| part.split(",").map(|num| num.parse().unwrap()).collect_tuple().unwrap()
+        ).collect_tuple().unwrap()
+    ).collect()
 }
 
 #[cfg(test)]
