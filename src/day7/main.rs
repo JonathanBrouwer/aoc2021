116b350aea119c8e7683f493c2e2500c3d7bc99a
Jonathan Brouwer <jonathantbrouwer@gmail.com>
2021-12-07 13:36:39 +0100

    Day 7

---
+++ src/day7/main.rs
@@ -0,0 +1,54 @@
+fn part1(inp: &str) -> usize {
+    let mut input = parse_input(inp);
+    input.sort();
+    let target = input[input.len() / 2];
+    input.iter().map(|x| x.abs_diff(target)).sum()
+}
+
+fn part2(inp: &str) -> usize {
+    let mut input = parse_input(inp);
+    let s: usize = input.iter().sum();
+    let cost = |target: usize| input.iter().map(|x| {
+        let diff = x.abs_diff(target);
+        diff * (diff + 1) / 2
+    }).sum();
+    return usize::min(cost(s / input.len()), cost((s + input.len() - 1) / input.len()));
+}
+
+fn parse_input(inp: &str) -> Vec<usize> {
+    inp.split(",").map(|n| n.parse().unwrap()).collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1_ex1() {
+        let result = part1(include_str!("example"));
+        assert_eq!(37, result);
+    }
+
+    #[test]
+    fn test_part1_real() {
+        let result = part1(include_str!("input"));
+        println!("Part 1: {}", result);
+        assert_eq!(359648, result);
+    }
+
+    #[test]
+    fn test_part2_ex1() {
+        let result = part2(include_str!("example"));
+        assert_eq!(168, result);
+    }
+
+    #[test]
+    fn test_part2_real() {
+        let result = part2(include_str!("input"));
+        println!("Part 2: {}", result);
+        assert_eq!(100727924, result);
+    }
+}
+
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 #![feature(iter_advance_by)]
+#![feature(int_abs_diff)]
+
 mod template;
 mod day1;
 mod day2;
@@ -6,6 +8,7 @@ mod day3;
 mod day4;
 mod day5;
 mod day6;
+mod day7;
 
 fn main() {
 
